03b623532d3b9f64b900856a06d0cd7c6a50e63e
CloudBudget Inc <68500411+CloudBudgetInc@users.noreply.github.com>
2024-05-16 22:31:30 +0200

    Updated version

--- src/lwc/cbTaxThresholds/cbTaxThresholds.js
+++ src/lwc/cbTaxThresholds/cbTaxThresholds.js
@@ -1,30 +1,5 @@
-/**
-Copyright (c) 04 2024, AJR
-All rights reserved.
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
- * Neither the name of the CloudBudget, Inc. nor the names of its contributors
-may be used to endorse or promote products derived from this software
-without specific prior written permission.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-OF THE POSSIBILITY OF SUCH DAMAGE.
-
- */
-import {LightningElement, track} from 'lwc';
-import {_applyDecStyle, _parseServerError} from "c/cbUtils";
+import { LightningElement, track } from 'lwc';
+import { _applyDecStyle, _parseServerError } from "c/cbUtils";
 import getTaxThresholdsServer from '@salesforce/apex/CBPayrollExpressPageController.getTaxThresholdsServer';
 import saveTaxThresholdsServer from '@salesforce/apex/CBPayrollExpressPageController.saveTaxThresholdsServer';
 import saveNFLibraryServer from '@salesforce/apex/CBPayrollExpressPageController.saveNFLibraryServer';
@@ -32,61 +7,78 @@ import addTaxThresholdServer from '@salesforce/apex/CBPayrollExpressPageControll
 import deleteTaxThresholdServer from '@salesforce/apex/CBPayrollExpressPageController.deleteTaxThresholdServer';
 
 export default class CBTaxThresholds extends LightningElement {
-
 	@track showSpinner = false;
 	@track readyToRender = false;
 	@track taxThresholds = [];
 
 	async connectedCallback() {
-		this.readyToRender = false;
 		this.showSpinner = true;
-		await this.getTaxThresholds();
-		this.showSpinner = false;
-		this.readyToRender = true;
-		_applyDecStyle();
-	};
+		try {
+			await this.getTaxThresholds();
+			this.readyToRender = true;
+			_applyDecStyle();
+		} catch (error) {
+			_parseServerError('Initialization Error: ', error);
+		} finally {
+			this.showSpinner = false;
+		}
+	}
 
-	getTaxThresholds = async () => {
+	async getTaxThresholds() {
 		try {
 			this.taxThresholds = await getTaxThresholdsServer();
-		} catch (e) {
-			_parseServerError('Get Employee Error: ', e);
+		} catch (error) {
+			_parseServerError('Get Tax Thresholds Error: ', error);
 		}
-	};
+	}
 
-	handleAnalyticChange = async (event) => {
+	async handleAnalyticChange(event) {
 		const tt = this.taxThresholds.find(t => t.Id === event.target.label);
 		tt[event.target.name] = event.target.value;
-		await saveTaxThresholdsServer({nflLib: tt});
-		this.connectedCallback();
-	};
+		try {
+			await saveTaxThresholdsServer({ nflLib: tt });
+		} catch (error) {
+			_parseServerError('Save Tax Threshold Error: ', error);
+		}
+		await this.connectedCallback();
+	}
 
-	handleTTChange = (event) => {
+	handleTTChange(event) {
 		const library = this.taxThresholds.find(tt => tt.Id === event.target.name);
 		library[event.target.label] = event.target.value;
-		saveNFLibraryServer({library});
-	};
+		saveNFLibraryServer({ library });
+	}
 
-	handleParamsChange = (event) => {
-		let field = event.target.label;
-		field = field.includes('Val') ? `cb5__${field}__c` : `${field}__c`;
+	handleParamsChange(event) {
+		const field = event.target.label.includes('Val') ? `cb5__${event.target.label}__c` : `${event.target.label}__c`;
 		const value = event.target.value;
 		const nflId = event.target.name;
-		const library = {Id: nflId};
+		const library = { Id: nflId };
 		library[field] = value;
-		saveNFLibraryServer({library});
-	};
+		saveNFLibraryServer({ library });
+	}
 
-	addTaxThreshold = async () => {
+	async addTaxThreshold() {
 		this.showSpinner = true;
-		await addTaxThresholdServer();
-		this.connectedCallback();
-	};
+		try {
+			await addTaxThresholdServer();
+			await this.connectedCallback();
+		} catch (error) {
+			_parseServerError('Add Tax Threshold Error: ', error);
+		} finally {
+			this.showSpinner = false;
+		}
+	}
 
-	deleteTaxThreshold = async (event) => {
+	async deleteTaxThreshold(event) {
 		this.showSpinner = true;
-		await deleteTaxThresholdServer({nflId: event.target.value});
-		this.connectedCallback();
-	};
-
+		try {
+			await deleteTaxThresholdServer({ nflId: event.target.value });
+			await this.connectedCallback();
+		} catch (error) {
+			_parseServerError('Delete Tax Threshold Error: ', error);
+		} finally {
+			this.showSpinner = false;
+		}
+	}
 }
